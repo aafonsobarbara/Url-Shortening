73804694c426eea66d4be02089ff857c7438a976
Barbara Luana <babiafonso11@gmail.com>
2021-04-07 16:09:32 -0300

    site finalizado

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ const btnShort = document.querySelector('.btn-short');
 const error = document.querySelector('.error');
 const input = document.getElementById('input');
 
-const hide = document.querySelector('.hide');
-const linkAntigo = document.querySelector('.url');
-const short_url = document.querySelector('.short-url');
-const btnCopy = document.querySelector('.btn-url');
+const linkAntigo = document.querySelector('.old-url');
+const shortenContent = document.querySelector('.shorten-content');
 function menuMobile() {
   menuHamburguer.addEventListener('click', function (e) {
     e.preventDefault();
@@ -19,18 +17,46 @@ function shortLink() {
   btnShort.addEventListener('click', function (e) {
     if (input.value == '') {
       error.style.visibility = 'initial';
-    } else {
+    } else if (input.value !== '') {
       error.style.visibility = 'hidden';
       const url = `https://api.shrtco.de/v2/shorten?url=${input.value}`;
       fetch(url)
         .then((response) => response.json())
         .then((link) => {
-          hide.style.display = 'block';
-          linkAntigo.innerText = input.value;
-          short_url.innerText = link.result.short_link;
+          const urls = document.createElement('div');
+          urls.classList.add('urls');
+          shortenContent.appendChild(urls);
+          const short_url = link.result.short_link;
+          urls.innerHTML = `
+          <p class="old-url">${input.value}</p>
+          <div class="box-urls">
+            <p class="short-url">${short_url}</p>
+            <button class="btn-copy" value="${short_url}">Copy</button>
+          </div>
+          `;
+
+          let btn_copy = document.querySelectorAll('.btn-copy');
+          btn_copy.forEach((btn) => {
+            btn.addEventListener('click', async function copy() {
+              const short_url = btn.value;
+              await navigator.clipboard.writeText(short_url);
+            });
+          });
+          localStorage.setItem(Math.floor(Math.random() * 100), urls.innerHTML);
         });
     }
   });
+  for (var i = 0; i < localStorage.length; i++) {
+    let saved = localStorage.getItem(localStorage.key(i));
+    if (saved) {
+      const urls = document.createElement('div');
+      urls.classList.add('urls');
+      urls.innerHTML = saved;
+      // var parser = new DOMParser();
+      // saved = parser.parseFromString(saved, 'text/html');
+      shortenContent.appendChild(urls);
+    }
+  }
 }
 
 menuMobile();
